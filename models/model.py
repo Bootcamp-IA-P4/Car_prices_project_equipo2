fb2473c65f14e52614a487225ff28e18353a78bc
Nho89 <noa1720@gmail.com>
2025-04-24 13:02:23 +0200

    feat: added model and pipeline

--- models/model.py
+++ models/model.py
@@ -1,10 +1,15 @@
-import joblib
-import pandas as pd
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
 
 
-def load_pipeline():
-    pipeline = joblib.load("models/model_pipeline.pkl")  
-    return pipeline
+##  dividir datos en conjuntos de entrenamiento y prueba.
+def split_data(df, features, target, test_size=0.2, random_state=42):
+    X_train, X_test, y_train, y_test = train_test_split(
+        df[features],
+        df[target],
+        test_size=test_size,
+        random_state=random_state
+    )
+    return X_train, X_test, y_train, y_test
+
