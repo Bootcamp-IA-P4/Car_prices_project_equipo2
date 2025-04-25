2dc5a4552b2219fe8b348263e2fde0876c7074cf
Nho89 <noa1720@gmail.com>
2025-04-25 17:26:48 +0200

    fix: updated data

--- app/app.py
+++ app/app.py
@@ -2,20 +2,13 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import joblib  # Importar joblib para cargar el modelo
+import joblib 
+import json
 
 
-brands = ['MINI', 'Lincoln', 'Chevrolet', 'Genesis', 'Mercedes-Benz', 'Audi',
-          'Ford', 'BMW', 'Tesla', 'Cadillac', 'Land', 'GMC', 'Toyota',
-          'Hyundai', 'Volvo', 'Volkswagen', 'Buick', 'Rivian', 'RAM',
-          'Hummer', 'Alfa', 'INFINITI', 'Jeep', 'Porsche', 'McLaren',
-          'Honda', 'Lexus', 'Dodge', 'Nissan', 'Jaguar', 'Acura', 'Kia',
-          'Mitsubishi', 'Rolls-Royce', 'Maserati', 'Pontiac', 'Saturn',
-          'Bentley', 'Mazda', 'Subaru', 'Ferrari', 'Aston', 'Lamborghini',
-          'Chrysler', 'Lucid', 'Lotus', 'Scion', 'smart', 'Karma',
-          'Plymouth', 'Suzuki', 'FIAT', 'Saab', 'Bugatti', 'Mercury',
-          'Polestar', 'Maybach']
-
+json_path = "brand_model_mapping.json"
+with open(json_path, 'r', encoding='utf-8') as json_file:
+    brand_model_mapping = json.load(json_file)
 ##Importar el modelo entrenado y el label encoder
 model = joblib.load("models/gradient_boosting_model.pkl")  # comprobar modelo y ruta
 label_encoder = joblib.load("models/label_encoder.pkl")
@@ -23,25 +16,31 @@ label_encoder = joblib.load("models/label_encoder.pkl")
 st.sidebar.header("Ingrese las características del coche")
 
 # inputs
-brand = st.sidebar.selectbox("Marca", options=brands)
-# model_car = st.sidebar.text_input("Modelo del coche", value="")
+brand = st.sidebar.selectbox("Marca del coche", options=list(brand_model_mapping.keys()))
+if brand:
+    model_car = st.sidebar.selectbox("Modelo del coche", options=brand_model_mapping[brand])
+
 model_year = st.sidebar.slider("Año del modelo", min_value=1970, max_value=2025, value=2015, step=1)
 milage = st.sidebar.number_input("Odómetro en millas", min_value=0, max_value=500000, value=50000, step=1000)
 transmission_num = st.sidebar.selectbox("Número de marchas", options=[1, 2, 4, 5, 6, 7, 8, 9, 10])
-
-
+accident = st.sidebar.selectbox("El coche ha sufrido algún accidente?", options=[0, 1])
+engine_hp = st.sidebar.number_input("Caballos de fuerza del motor", min_value=0, max_value=1000, value=150, step=10)
 
 if st.sidebar.button("Predecir precio"):
     enconded_brand = label_encoder.transform([brand])[0] 
+    enconded_model_car = label_encoder.transform([model_car])[0] 
      # Codificar la marca
     # Crea un DataFrame con las características ingresadas
     input_data = pd.DataFrame({
         "brand": [enconded_brand],
-        # "model": [model_car],  
+        "model": [enconded_model_car],  
         "model_year": [model_year],
         "milage": [milage],
-        "transmission_num": [transmission_num]
-    })
+        "transmission_num": [transmission_num],
+        "accident": [accident],
+        "engine_hp": [0],
+        })
+   
 
     
     predicted_price = model.predict(input_data)[0]
